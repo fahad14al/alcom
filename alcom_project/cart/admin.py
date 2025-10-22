9681ae1c1ef2bc91f212ab71dea836aa344900cc
fahad14al <fahadalmohammad50@gmail.com>
2025-10-22 14:15:28 +0600

    first commit

---
+++ alcom_project/cart/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Cart, CartItem, Coupon, Discount
+
+# Register your models here.
+admin.site.register(Cart)
+admin.site.register(CartItem)
+admin.site.register(Coupon)
+admin.site.register(Discount)
