8eaba836ee53d34c55cbcb930f847a1b6007326e
Al Mohammad Fahad <155531186+fahad14al@users.noreply.github.com>
2025-12-08 16:01:50 +0600

    Delete alcom directory

--- alcom/analytics/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import ProductView, PageView, SalesReport
-
-# Register your models here.
-admin.site.register(ProductView)
-admin.site.register(PageView)
-admin.site.register(SalesReport)
--- alcom/reviews/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Rating, Review, ReviewImage
-
-# Register your models here.
-admin.site.register(Rating)
-admin.site.register(Review)
-admin.site.register(ReviewImage)
